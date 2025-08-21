44d8ac8c4d195d8bde7559d1bebc412d41271a03
lee sua <s2332@e-mirim.hs.kr>
2025-08-21 16:03:05 +0900

    #1 사용자 입력 구현 일단 동작만 하게 프로토타이핑 먼저. 자세한 수정은 나중에 할 예정

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace FortuneTeller
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
