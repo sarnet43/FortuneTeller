5b48c31f8afac20bb16794d83d93c5919b12ba34
lee sua <s2332@e-mirim.hs.kr>
2025-08-27 13:42:49 +0900

    #2 중복으로 내역 불러오기 창이 뜨는 것을 수정
    내역 불러오기 기능 개선
    
    `내역불러오기ToolStripMenuItem_Click` 메서드에서
    `FormHistory` 인스턴스를 생성하는 방식을 변경했습니다.
    이제 열려 있는 `FormHistory` 폼을 확인하여 활성화하고,
    열려 있지 않을 경우에만 새로운 인스턴스를 생성하여 표시합니다.
    이로 인해 중복된 폼이 열리지 않도록 개선되었습니다.

--- Form1.cs
+++ Form1.cs
@@ -29,8 +29,17 @@ namespace FortuneTeller
 
         private void 내역불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormHistory form = new FormHistory();
-            form.Show();
+            FormHistory form = Application.OpenForms["FormHistory"] as FormHistory;
+            if (form != null)
+            {
+                form.Activate();
+            }
+            else
+            {
+                form = new FormHistory();
+                form.Show();
+            }
+                
         }
 
         private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
